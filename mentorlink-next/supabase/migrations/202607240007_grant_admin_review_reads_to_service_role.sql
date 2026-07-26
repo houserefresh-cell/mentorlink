901b150ebf25eaeb18ecacaee259b49b6f0cffcf
houserefresh-cell <house.refresh@gmail.com>
2026-07-26 21:05:52 +0300

    Grant admin review read access

---
+++ mentorlink-next/supabase/migrations/202607240007_grant_admin_review_reads_to_service_role.sql
@@ -0,0 +1,12 @@
+grant select
+on table
+  public.mentor_publication,
+  public.mentor_profiles,
+  public.mentor_subjects,
+  public.subjects,
+  public.mentor_availability,
+  public.mentor_locations,
+  public.mentor_experience,
+  public.mentor_preferences,
+  public.mentor_parent_consents
+to service_role;
